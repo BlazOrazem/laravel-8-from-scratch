1cca912f8706b595d001573d849cf922c4ea4e70
Blaz Orazem <blaz@orazem.si>
2022-03-17 15:36:58 +0100

    Episode 7

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas imperdiet sagittis accumsan. Sed posuere
+        bibendum neque quis porttitor. Sed quis ultrices augue. Integer eget aliquet ligula, eget fermentum leo.
+        Curabitur gravida consequat nibh, dapibus tempus purus molestie at. Nullam non dolor erat. Vestibulum sed dui
+        semper, congue sapien vel, consequat urna. Duis accumsan posuere mollis. Nunc accumsan venenatis enim, at
+        hendrerit elit vehicula non. Integer elementum nisl in consequat elementum. Pellentesque et orci porttitor,
+        mollis nulla a, egestas turpis. Aliquam sagittis ac sapien a posuere.
+    </p>
+</article>
+<article>
+    <h1><a href="/post">My Second Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas imperdiet sagittis accumsan. Sed posuere
+        bibendum neque quis porttitor. Sed quis ultrices augue. Integer eget aliquet ligula, eget fermentum leo.
+        Curabitur gravida consequat nibh, dapibus tempus purus molestie at. Nullam non dolor erat. Vestibulum sed dui
+        semper, congue sapien vel, consequat urna. Duis accumsan posuere mollis. Nunc accumsan venenatis enim, at
+        hendrerit elit vehicula non. Integer elementum nisl in consequat elementum. Pellentesque et orci porttitor,
+        mollis nulla a, egestas turpis. Aliquam sagittis ac sapien a posuere.
+    </p>
+</article>
+<article>
+    <h1><a href="/post">My Third Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas imperdiet sagittis accumsan. Sed posuere
+        bibendum neque quis porttitor. Sed quis ultrices augue. Integer eget aliquet ligula, eget fermentum leo.
+        Curabitur gravida consequat nibh, dapibus tempus purus molestie at. Nullam non dolor erat. Vestibulum sed dui
+        semper, congue sapien vel, consequat urna. Duis accumsan posuere mollis. Nunc accumsan venenatis enim, at
+        hendrerit elit vehicula non. Integer elementum nisl in consequat elementum. Pellentesque et orci porttitor,
+        mollis nulla a, egestas turpis. Aliquam sagittis ac sapien a posuere.
+    </p>
+</article>
+</body>
